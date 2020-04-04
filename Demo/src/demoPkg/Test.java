f00fbf91222cf69c98cbf31a0ab1e41d6489b9b0
Sudipta Sahoo <Sudipta Sahoo@192.168.1.215>
2020-04-04 21:41:48 +0530

    New Commit for Test

--- Demo/src/demoPkg/Test.java
+++ Demo/src/demoPkg/Test.java
@@ -12,10 +12,18 @@ public class Test {
 		int c = a+b;
 		System.out.println(c);
 		
+		Test t = new Test();
+		t.gitdemo();
+		
 		System.setProperty("webdriver.chrome.driver", "D:\\selenium_setup\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 
+	}
+	
+	public void gitdemo()
+	{
+		System.out.println("First Git chanage from Eclipse");
 	}
 
 }
