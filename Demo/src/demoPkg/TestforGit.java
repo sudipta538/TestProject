af8d34d509fc3864baaf8d503423860135e31d5f
chandrakanta <chandrakanta@192.168.1.218>
2020-04-05 00:32:33 +0530

    Test for Push as Collaborator

---
+++ Demo/src/demoPkg/TestforGit.java
@@ -0,0 +1,12 @@
+package demoPkg;
+
+public class TestforGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Git as Collaorator");
+		System.out.println("Git success");
+
+	}
+
+}
